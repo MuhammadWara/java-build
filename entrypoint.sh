30a0b59293b553eae848d4b3da1af2de50f2457e
Muhammad Wara Bin Wafa <mwara@nisum.com>
2025-02-07 16:16:16 +0500

    adding custom action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-# Ensure that the Maven build command is executed in the correct directory
 echo "Running Maven Build and Test..."
-
-# Pull in the repository from the GitHub runner
-cd $GITHUB_WORKSPACE
-
-# Run Maven clean, install, and test
-mvn clean install
-
-# Run the unit tests
-mvn test
-
+cd $GITHUB_WORKSPACE  # Move to the repository workspace
+mvn clean install     # Build the Java project
+mvn test              # Run tests
 echo "Build and Test complete!"
